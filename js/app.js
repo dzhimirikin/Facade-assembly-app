3514a55e8891097dc6cf664f6fef080b11439a60
dzhimirikin <d.zhimirikin@gmail.com>
2026-05-22 20:14:13 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyA7cHiDj4BAMbw1LoTpgbxXIcrhDnm0lX8",
+  authDomain: "facade-assembly.firebaseapp.com",
+  projectId: "facade-assembly",
+  storageBucket: "facade-assembly.firebasestorage.app",
+  messagingSenderId: "676074892011",
+  appId: "1:676074892011:web:792ebcdf46e2098f972cae",
+  measurementId: "G-ZVSZ1L3FQZ"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const elementId = document.querySelector("#elementId").value;
+  const stage = document.querySelector("#stage").value;
+  const employee = document.querySelector("#employee").value;
+
+  await addDoc(collection(db, "assemblyLogs"), {
+    elementId,
+    stage,
+    employee,
+    timestamp: new Date()
+  });
+
+  alert("Saved to Firebase");
+});
