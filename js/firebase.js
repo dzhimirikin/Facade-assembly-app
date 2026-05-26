b771b57cd056655cadb5240fac359a4abae5e3c4
dzhimirikin <d.zhimirikin@gmail.com>
2026-05-26 16:59:51 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -8,17 +8,22 @@ import {
 
 const firebaseConfig = {
 
-  apiKey: "ВАШ_API_KEY",
+  apiKey: "AIzaSyA7cHiDj4BAMbw1LoTpgbxXIcrhDnm0lX8",
 
-  authDomain: "ВАШ_AUTH_DOMAIN",
+  authDomain:
+    "facade-assembly.firebaseapp.com",
 
-  projectId: "ВАШ_PROJECT_ID",
+  projectId:
+    "facade-assembly",
 
-  storageBucket: "ВАШ_STORAGE_BUCKET",
+  storageBucket:
+    "facade-assembly.firebasestorage.app",
 
-  messagingSenderId: "ВАШ_MESSAGING_SENDER_ID",
+  messagingSenderId:
+    "676074892011",
 
-  appId: "ВАШ_APP_ID"
+  appId:
+    "1:676074892011:web:792ebcdf46e2098f972cae"
 
 };
 
